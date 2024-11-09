e96cde51c56a0ae499cd4085d0d4ddef69bf191f
Awin Gray <grayawin@gmail.com>
2024-11-09 11:03:29 +0700

    added MessageResponse

---
+++ Boonkrua/Models/Response/MessageResponse.cs
@@ -0,0 +1,10 @@
+namespace Boonkrua.Models.Response;
+
+public sealed record MessageResponse
+{
+    public string Message { get; }
+
+    private MessageResponse(string message) => Message = message;
+
+    public static MessageResponse Create(string message) => new(message);
+}
