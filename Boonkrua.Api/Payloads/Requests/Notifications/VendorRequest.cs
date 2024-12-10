3b29729a19cfd0a0ee196f1a9e6d2abbedad6aca
Awin Gray <grayawin@gmail.com>
2024-12-10 11:09:59 +0700

    changed notification type to string and parse in mapper to make it clearer for frontend

--- Boonkrua.Api/Payloads/Requests/Notifications/VendorRequest.cs
+++ Boonkrua.Api/Payloads/Requests/Notifications/VendorRequest.cs
@@ -1,13 +1,23 @@
 using Boonkrua.Api.Interfaces;
 using Boonkrua.Service.Models.Dto;
 using Boonkrua.Shared.Enums;
+using Boonkrua.Shared.Extensions;
+using Boonkrua.Shared.Messages;
 
 namespace Boonkrua.Api.Payloads.Requests.Notifications;
 
 public sealed record VendorRequest : IRequestMapper<VendorDto>
 {
-    public NotificationType Type { get; init; }
+    public required string Type { get; init; }
     public Dictionary<string, string> Config { get; init; } = [];
 
-    public VendorDto ToDto() => VendorDto.Create(Type, Config);
+    public VendorDto ToDto()
+    {
+        if (!Type.TryParse(out NotificationType type))
+        {
+            throw new InvalidOperationException(NotificationConfigMessages.InvalidNotificationType);
+        }
+
+        return VendorDto.Create(type, Config);
+    }
 }
---
+++ Boonkrua.Shared/Messages/NotificationConfigMessages.cs
@@ -0,0 +1,6 @@
+namespace Boonkrua.Shared.Messages;
+
+public static class NotificationConfigMessages
+{
+    public const string InvalidNotificationType = "The notification type is invalid.";
+}
