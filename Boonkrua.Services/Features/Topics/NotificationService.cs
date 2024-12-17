cb61a78a1e00ac7d352f97e2c77b8de8fea2ef71
Awin Gray <grayawin@gmail.com>
2024-12-17 21:57:56 +0700

    used other services in the notification service layer

--- Boonkrua.Services/Features/Topics/NotificationService.cs
+++ Boonkrua.Services/Features/Topics/NotificationService.cs
@@ -1,50 +1,46 @@
 using Boonkrua.Data.Features.Configs.Interfaces;
 using Boonkrua.Data.Features.Topics.Interfaces;
+using Boonkrua.Services.Features.Configs.Interfaces;
 using Boonkrua.Services.Features.Notifications.Factories;
+using Boonkrua.Services.Features.Notifications.Interfaces;
 using Boonkrua.Services.Features.Notifications.Models;
 using Boonkrua.Services.Features.Topics.Interfaces;
 using Boonkrua.Shared.Abstractions;
 using Boonkrua.Shared.Enums;
 using Boonkrua.Shared.Extensions;
-using NotificationError = Boonkrua.Services.Features.Topics.Models.NotificationError;
+using INotificationService = Boonkrua.Services.Features.Topics.Interfaces.INotificationService;
 
 namespace Boonkrua.Services.Features.Topics;
 
 using NotificationError = Models.NotificationError;
 
 public sealed class NotificationService(
-    ITopicRepository topicRepository,
-    IConfigRepository configRepository,
-    NotificationServiceFactory serviceFactory
+    ITopicService topicService,
+    IConfigService configService,
+    INotificationDispatcher dispatcher
 ) : INotificationService
 {
-    private readonly ITopicRepository _topicRepository = topicRepository;
-    private readonly IConfigRepository _configRepository = configRepository;
-    private readonly NotificationServiceFactory _serviceFactory = serviceFactory;
+    private readonly ITopicService _topicService = topicService;
+    private readonly IConfigService _configService = configService;
+    private readonly INotificationDispatcher _dispatcher = dispatcher;
 
     public async Task<Result<Message, NotificationError>> NotifyAsync(string objectId, string type)
     {
         if (!type.TryParse(out NotificationType notificationType))
             return NotificationError.InvalidType;
 
-        var topic = await _topicRepository.GetByIdAsync(objectId);
-        if (topic is null)
+        var topic = await _topicService.GetByIdAsync(objectId);
+        if (!topic.IsSuccessful)
             return NotificationError.NotFound;
 
-        var userConfigs = await _configRepository.GetByUserIdAsync(topic.UserId);
-        if (userConfigs is null)
-            return NotificationError.NotFoundUser;
+        var (userId, title) = (topic.Content!.UserId, topic.Content!.Title);
 
-        var vendorConfig = userConfigs.GetKeyByType(notificationType);
-        if (vendorConfig is null)
+        var config = await _configService.GetVendorKeyByTypeAsync(userId, notificationType);
+        if (!config.IsSuccessful)
             return NotificationError.NotFoundConfig;
 
-        var notificationService = _serviceFactory.GetService(notificationType);
-        var payload = NotificationPayload.Create(topic.Title, vendorConfig);
-
-        var result = await notificationService.SendNotificationAsync(payload);
-        if (!result.IsSuccessful)
-            return NotificationError.SendFailure;
+        var payload = NotificationPayload.Create(title, config.Content!);
+        var result = await _dispatcher.DispatchAsync(notificationType, payload);
 
         return result.Content!;
     }
