13b09c502182bfed719efed59f7be6dd457378f8
Awin Gray <grayawin@gmail.com>
2024-12-17 20:15:29 +0700

    encapsulate db configuration by introducing private fields

--- Boonkrua.Data/Contexts/BoonkruaContext.cs
+++ Boonkrua.Data/Contexts/BoonkruaContext.cs
@@ -4,13 +4,20 @@ using MongoDB.Driver;
 
 namespace Boonkrua.Data.Contexts;
 
-public sealed class BoonkruaContext(IMongoClient client, string dbName)
+public sealed class BoonkruaContext
 {
-    private readonly IMongoDatabase _db = client.GetDatabase(dbName);
+    private readonly IMongoCollection<Topic> _topics;
+    private readonly IMongoCollection<Config> _configs;
 
-    public IMongoCollection<Topic> Topics => _db.GetCollection<Topic>(nameof(Topics));
+    public BoonkruaContext(IMongoClient client, string dbName)
+    {
+        var db = client.GetDatabase(dbName);
+        _topics = db.GetCollection<Topic>(nameof(Topics));
+        _configs = db.GetCollection<Config>(nameof(Configs));
+    }
 
-    public IMongoCollection<Config> Configs => _db.GetCollection<Config>(nameof(Configs));
+    public IMongoCollection<Topic> Topics => _topics;
+    public IMongoCollection<Config> Configs => _configs;
 
     public void EnsureIndexes()
     {
@@ -23,6 +30,6 @@ public sealed class BoonkruaContext(IMongoClient client, string dbName)
         var indexKeys = Builders<Config>.IndexKeys.Ascending(config => config.UserId);
         var indexModel = new CreateIndexModel<Config>(indexKeys, indexOptions);
 
-        Configs.Indexes.CreateOne(indexModel);
+        _configs.Indexes.CreateOne(indexModel);
     }
 }
