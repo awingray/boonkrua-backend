e4b8a621adf8c3a379a97a8d71c1ad66d71f12ce
Awin Gray <grayawin@gmail.com>
2024-12-19 21:53:29 +0700

    added serviceResult

---
+++ Boonkrua.Services/Models/ServiceResult.cs
@@ -0,0 +1,29 @@
+using Boonkrua.Shared.Abstractions;
+
+namespace Boonkrua.Services.Models;
+
+public class ServiceResult<TContent>
+{
+    private readonly Result<TContent, AError> _result;
+
+    private ServiceResult(TContent content)
+    {
+        _result = Result<TContent, AError>.Ok(content);
+    }
+
+    private ServiceResult(AError error)
+    {
+        _result = Result<TContent, AError>.Err(error);
+    }
+
+    public static implicit operator ServiceResult<TContent>(TContent content) => new(content);
+
+    public static implicit operator ServiceResult<TContent>(AError error) => new(error);
+
+    public TReturn Match<TReturn>(
+        Func<TContent, TReturn> onSuccess,
+        Func<AError, TReturn> onFailure
+    ) => _result.Match(onSuccess, onFailure);
+
+    public bool IsSuccessful => _result.IsSuccessful;
+}
