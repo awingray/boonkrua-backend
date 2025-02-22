dcfc1daeab2ef330a864009f755cabd9202a16a3
Awin Gray <grayawin@gmail.com>
2025-02-22 16:18:14 +0700

    optimize ToMappedList

--- Boonkrua.Shared/Extensions/EnumerableExtensions.cs
+++ Boonkrua.Shared/Extensions/EnumerableExtensions.cs
@@ -5,7 +5,15 @@ public static class EnumerableExtensions
     public static List<TResult> ToMappedList<TSource, TResult>(
         this IEnumerable<TSource> source,
         Func<TSource, TResult> mapper
-    ) => source?.Select(mapper).ToList() ?? [];
+    )
+    {
+        if (source is not ICollection<TSource> collection)
+            return [.. source.Select(mapper)];
+
+        var result = new List<TResult>(collection.Count);
+        result.AddRange(collection.Select(mapper));
+        return result;
+    }
 
     public static bool IsNullOrEmpty<T>(this IEnumerable<T>? source) =>
         source is null || !source.Any();
